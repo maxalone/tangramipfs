e20ef84772fa560dd1eaac131392110c804f3677
maxAlone <mascam64@gmail.com>
2022-08-24 14:48:19 +0200

    kubodim

--- js/scripts.js
+++ js/scripts.js
@@ -51,4 +51,32 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+
+
+
+
+
+
+
+
 });
+
+
+
+function kubodim(){
+    var url = "https://reqbin.com/echo/get/json";
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", url);
+
+xhr.setRequestHeader("Accept", "application/json");
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+xhr.send();
+document.getElementById("kubodim").textContent=xhr.responseText;
+}
