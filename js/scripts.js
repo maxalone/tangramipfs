ca624bb71613221943c9ce83c1f35ad09731cff0
maxAlone <mascam64@gmail.com>
2022-08-24 23:31:37 +0200

    new counter

--- js/scripts.js
+++ js/scripts.js
@@ -83,3 +83,19 @@ xhr.onreadystatechange = function () {
 
 xhr.send();
 }
+
+
+ 
+ $(document).ready(function(){
+     $('.counter-value').each(function(){
+         $(this).prop('Counter',0).animate({
+             Counter: $(this).text()
+         },{
+             duration: 3500,
+             easing: 'swing',
+             step: function (now){
+                 $(this).text(Math.ceil(now));
+             }
+         });
+     });
+ });
